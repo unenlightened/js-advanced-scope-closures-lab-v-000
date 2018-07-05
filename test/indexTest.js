a9b667ffa7b14658477443d0924548125e43cdfd
Chely Ho <adept_inept@yahoo.com>
2018-07-05 19:39:30 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,35 +1,7 @@
 const expect = chai.expect;
 
 describe('closures', function() {
-  describe('produceDrivingRange', function(){
-    it('returns a function', function(){
-      expect(typeof produceDrivingRange()).to.equal('function')
-    })
-
-    it('takes an argument of blockRange which is then used to calculate if something is within range', function(){
-      let eightBlockRange = produceDrivingRange(8)
-      expect(eightBlockRange('10th', '20th')).to.equal('2 blocks out of range')
-      expect(eightBlockRange('10th', '14th')).to.equal('within range by 4')
-    })
-
-    it('returns when something is out of range', function(){
-      let twentyBlockRange = produceDrivingRange(20)
-      expect(twentyBlockRange('10th', '20th')).to.equal('within range by 10')
-      expect(twentyBlockRange('10th', '40th')).to.equal('10 blocks out of range')
-    })
-  })
-
-  describe('produceTipCalculator', function(){
-    it('returns a function', function(){
-      expect(typeof produceTipCalculator()).to.equal('function')
-    })
-
-    it('configures the percentage tip returned', function(){
-      let tenPercentTip = produceTipCalculator(.10)
-      expect(tenPercentTip(50)).to.equal(5)
-      expect(tenPercentTip(30)).to.equal(3)
-    })
-  })
+  
 
   describe('createDriver', function(){
     const Driver = createDriver()

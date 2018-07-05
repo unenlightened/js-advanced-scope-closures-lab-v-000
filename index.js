6d0020d4f33b8150cdc0c41f706194133b63a0ab
Chely Ho <adept_inept@yahoo.com>
2018-07-05 19:09:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const produceDrivingRange = function(blockRange) {
-  return blockRange <= 10
+function produceDrivingRange(blockRange) {
+  return function(blockRange) {
+    return blockRange <= 10
+  }
 }

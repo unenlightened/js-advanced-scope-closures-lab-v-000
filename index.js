dbb6e1be57cb856f5025623fe28def8df43749a3
Chely Ho <adept_inept@yahoo.com>
2018-07-05 19:43:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ function produceTipCalculator(percentage) {
     return fare * percentage
   }
 }
+
+function createDriver(name) {
+  let DriverId = 0
+
+  return class {
+    constructor(name) {
+      this.id = DriverId++
+      this.name = name
+    }
+  }
+}
+
+describe('createDriver', function(){
+  const Driver = createDriver()
+
+  it('returns a class that allows us to create a driver with a name attribute', function(){
+    expect(new Driver('Sam').name).to.equal('Sam')
+  })
+
+  it('increments the driverId each time a driver is created', function(){
+    expect(new Driver('Sam').id).to.not.equal(new Driver('Bob').id)
+  })
+})

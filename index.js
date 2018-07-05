f35bc11605375b492e680d81f1da2b9b2e2f4274
Chely Ho <adept_inept@yahoo.com>
2018-07-05 19:43:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,15 +21,3 @@ function createDriver(name) {
     }
   }
 }
-
-describe('createDriver', function(){
-  const Driver = createDriver()
-
-  it('returns a class that allows us to create a driver with a name attribute', function(){
-    expect(new Driver('Sam').name).to.equal('Sam')
-  })
-
-  it('increments the driverId each time a driver is created', function(){
-    expect(new Driver('Sam').id).to.not.equal(new Driver('Bob').id)
-  })
-})
